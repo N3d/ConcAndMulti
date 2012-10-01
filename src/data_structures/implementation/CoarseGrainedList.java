ca5b604e0c1f9b69aa80d9b54ad110b2566148a9
Carmine Paolino <carmine@paolino.me>
2012-10-01 17:49:30 +0200

    CoarseGrainedList

--- src/data_structures/implementation/CoarseGrainedList.java
+++ src/data_structures/implementation/CoarseGrainedList.java
@@ -1,18 +1,75 @@
 package data_structures.implementation;
 
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import data_structures.Sorted;
 
 public class CoarseGrainedList<T extends Comparable<T>> implements Sorted<T> {
 
-	public void add(T t) {
-		throw new UnsupportedOperationException();
+	private Element<T> head;
+	private Lock lock = new ReentrantLock();
+
+	public CoarseGrainedList() {
+		head = new Element<T>();
+		head.next = new Element<T>();
 	}
 
-	public void remove(T t) {
-		throw new UnsupportedOperationException();
+	public void add(T item) {
+		Element<T> pred, curr;
+		lock.lock();
+		try {
+			pred = head;
+			curr = pred.next;
+			while (curr.value != null && curr.value.compareTo(item) == -1) {
+				pred = curr;
+				curr = curr.next;
+			}
+			Element<T> elem = new Element<T>(item);
+			elem.next = curr;
+			pred.next = elem;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public void remove(T item) {
+		Element<T> pred, curr;
+		lock.lock();
+		try {
+			pred = head;
+			curr = pred.next;
+			while (curr.getValue().compareTo(item) == -1) {
+				pred = curr;
+				curr = curr.next;
+			}
+			pred.next = curr.next;
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	public String toString() {
-		throw new UnsupportedOperationException();
+		return this.toArrayList().toString();
+	}
+
+	private ArrayList<T> toArrayList() {
+		Element<T> pred, curr;
+		ArrayList<T> array = new ArrayList<T>();
+//		lock.unlock();
+//		try {
+			pred = head;
+			curr = pred.next;
+			while (curr.next != null) {
+				array.add(curr.value);
+				pred = curr;
+				curr = curr.next;
+			}
+
+			return array;
+//		} finally {
+//			lock.unlock();
+//		}
 	}
 }
--- src/data_structures/implementation/Element.java
+++ src/data_structures/implementation/Element.java
@@ -1,12 +1,13 @@
 package data_structures.implementation;
 /**
- * @author Matteo Casenove and Nicola Mularoni
+ * @author Matteo Casenove, Nicola Mularoni, and Carmine Paolino
  * 
  * This class represents the generic element in the list.
  *
  */
 
 public class Element<T extends Comparable<T>> implements Comparable<Element<T>>{
+<<<<<<< HEAD
 	
 	static final int ELEMENT = 0;
 	static final int HEAD = 1;
@@ -16,18 +17,29 @@ public class Element<T extends Comparable<T>> implements Comparable<Element<T>>{
 	public Element<T> next;
 	private int type;
 	
+=======
+
+	public T value;
+	public Element<T> next;
+
+>>>>>>> CoarseGrainedList
 	public Element(){
 		this.value=null;
 		this.next=null;
 	}
-	
+
 	public Element(T value){
 		this.value=value;
 		this.type=ELEMENT;
 		this.next=null;
 	}
+<<<<<<< HEAD
 	
 	public Element(T value, int type){
+=======
+
+	public Element(T value, Element<T> next){
+>>>>>>> CoarseGrainedList
 		this.value=value;
 		this.type=type;
 		this.next=null;
@@ -48,25 +60,29 @@ public class Element<T extends Comparable<T>> implements Comparable<Element<T>>{
 	public boolean isTail(){
 		return (this.type==Element.TAIL);
 	}
-	
+
 	public T getValue(){
 		return this.value;
 	}
-	
+
 	public boolean isNull(){
 		return (this.value==null);
 	}
-	
+
 	public Element<T> getNext(){
 		return this.next;
 	}
-	
+
 	public void setValue(T value){
 		this.value=value;
 	}
-	
+
 	public void setNext(Element<T> next){
 		this.next=next;
+	}
+
+	public String toString() {
+		return value.toString();
 	}
 
 	@Override
